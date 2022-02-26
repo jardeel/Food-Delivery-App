527294bc7b8969b458c072814ba943cdcd57b1e4
Jardel Sousa <jardeelsousa@hotmail.com>
2022-02-25 22:12:27 -0300

    Created FilterModal for HomeScreen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -8,7 +8,7 @@ import {
   FlatList
 } from 'react-native';
 
-import Search from './components/Search';
+import FilterModal from './FilterModal';
 import { HorizontalFoodCard, VerticalFoodCard } from '../../components';
 
 import { FONTS, SIZES, COLORS, icons, dummyData } from '../../constants';
@@ -52,6 +52,7 @@ const Home = () => {
   const [popular, setPopular] = useState([]);
   const [recommends, setRecommends] = useState([]);
   const [menuList, setMenuList] = useState([]);
+  const [showFilterModal, setShowFilterModal] = useState(false);
 
   useEffect(() => {
     handleChangeCategory(selectedCategoryId, selectedMenuType)
@@ -258,10 +259,71 @@ const Home = () => {
     )
   }
 
+  function renderSearch(){
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          height: 40,
+          alignItems: 'center',
+          marginHorizontal: SIZES.padding,
+          marginVertical: SIZES.base,
+          paddingHorizontal: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.lightGray2
+        }}
+      >
+        {/* Icon */}
+        <Image
+          source={icons.search}
+          style={{
+            height: 20,
+            width: 20,
+            tintColor: COLORS.black
+          }}
+        />
+
+        {/* Text Input */}
+        <TextInput
+          style={{
+            flex: 1,
+            marginLeft: SIZES.radius,
+            ...FONTS.body3,
+          }}
+          placeholder="search food..."
+        />
+
+        {/* Filter Button */}
+        <TouchableOpacity
+          onPress={() => setShowFilterModal(true)}
+        >
+          <Image
+            source={icons.filter}
+            style={{
+              height: 20,
+              width: 20,
+              tintColor: COLORS.black
+            }}
+          />
+
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={{ flex: 1}}>
       {/* Search */}
-      <Search />
+      {renderSearch()}
+
+      {/* Filter */}
+      {showFilterModal &&
+        <FilterModal
+          isVisible={showFilterModal}
+          onClose={() => setShowFilterModal(false)}
+        />
+      }
+
 
       {/* List */}
       <FlatList

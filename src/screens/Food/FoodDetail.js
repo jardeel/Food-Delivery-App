c1a458b4015c95c384d2e6416c92a47c3631a1be
Jardel Sousa <jardeelsousa@hotmail.com>
2022-03-01 22:20:07 -0300

    Created Details for FoodDetail Screen

--- src/screens/Food/FoodDetail.js
+++ src/screens/Food/FoodDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, ScrollView } from 'react-native';
 
 import {
@@ -13,6 +13,8 @@ import {
 import { HeaderDetail, IconButton, CartQuantityButton } from '../../components';
 
 const FoodDetail = ({ navigation }) => {
+  const [foodItem, setFoodItem] = useState(dummyData.vegBiryani);
+
 
   function renderHeaderDetail() {
     return (
@@ -52,12 +54,101 @@ const FoodDetail = ({ navigation }) => {
     )
   }
 
+  function renderDetails(){
+    return(
+      <View
+        style={{
+          marginTop: SIZES.radius,
+          marginBottom: SIZES.padding,
+          paddingHorizontal: SIZES.padding
+        }}
+      >
+        {/* Food Card */}
+        <View
+          style={{
+            height: 190,
+            borderRadius: 15,
+            backgroundColor: COLORS.lightGray2
+          }}
+        >
+          {/* Calories & Favourite */}
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: SIZES.base,
+              paddingHorizontal: SIZES.radius
+            }}
+          >
+            {/* Calories */}
+            <View style={{ flexDirection: 'row' }}>
+              <Image
+                source={icons.calories}
+                style={{
+                  width: 30,
+                  height: 30
+                }}
+              />
+              <Text style={{ color: COLORS.darkGray2, ...FONTS.body4 }}>
+                {foodItem?.calories} calories
+              </Text>
+            </View>
+
+            {/* Favourite */}
+            <Image
+              source={icons.love}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: foodItem?.isFavourite ? COLORS.primary : COLORS.gray
+              }}
+            />
+          </View>
+
+          {/* Food Image */}
+          <Image
+            source={foodItem?.image}
+            resizeMode="contain"
+            style={{
+              height: 170,
+              width: "100%"
+            }}
+          />
+        </View>
+
+        {/* Food Info */}
+        <View style={{ marginTop: SIZES.padding }}>
+          {/* Name & description */}
+          <Text style={{...FONTS.h1 }}>
+            {foodItem?.name}
+          </Text>
+          <Text
+            style={{
+              marginTop: SIZES.base,
+              color: COLORS.darkGray,
+              textAlign: 'justify',
+              ...FONTS.body3
+            }}
+          >
+            {foodItem?.description}
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.white }}>
       {/* Header */}
       {renderHeaderDetail()}
 
       {/* Body */}
+      <ScrollView>
+        {/* Food Detail */}
+        {renderDetails()}
+
+        {/* Restaurant */}
+      </ScrollView>
 
       {/* Footer */}
     </View>

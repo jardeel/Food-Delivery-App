d30cd462e9e716272e5a6c69b5e1dba3f2374bef
Jardel Sousa <jardeelsousa@hotmail.com>
2022-03-02 21:48:00 -0300

    Created Header for MyCart Screen

--- src/screens/Cart/MyCart.js
+++ src/screens/Cart/MyCart.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+import {
+  Header,
+  IconButton,
+  CartQuantityButton,
+  StepperInput
+} from '../../components';
+import { FONTS, SIZES, COLORS, icons, dummyData } from '../../constants';
 
 const MyCart = ({ navigation }) => {
+
+  function renderHeader() {
+    return (
+      <Header
+        title="MY CART"
+        containerStyle={{
+          height: 50,
+          marginHorizontal: SIZES.padding,
+          marginTop: 40
+        }}
+        leftComponent={
+          <IconButton
+            icon={icons.back}
+            containerStyle={{
+              width: 40,
+              height: 40,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderWidth: 1,
+              borderRadius: SIZES.radius,
+              borderColor: COLORS.gray2
+            }}
+            iconStyle={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.gray2
+            }}
+            onPress={() => navigation.goBack()}
+          />
+        }
+        rightComponent={
+          <CartQuantityButton
+            quantity={3}
+          />
+        }
+      />
+    )
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>MyCart</Text>
+    <View style={{ flex: 1, backgroundColor: COLORS.white }}>
+      {/* Header */}
+      {renderHeader()}
+
+      {/* Cart List */}
+
+      {/* Footer */}
     </View>
   )
 }

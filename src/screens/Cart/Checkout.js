c78e8935df2865d971f0e119ef9eb2202a2904d9
Jardel Sousa <jardeelsousa@hotmail.com>
2022-03-05 15:34:43 -0300

    Created Header for Checkout Screen

--- src/screens/Cart/Checkout.js
+++ src/screens/Cart/Checkout.js
@@ -1,11 +1,66 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { COLORS } from '../../constants';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+import {
+  Header,
+  IconButton,
+  FormInput,
+  CardItem,
+  FooterTotal
+} from '../../components';
+import { FONTS, SIZES, COLORS, icons, dummyData } from '../../constants';
 
 const Checkout = ({ navigation, route }) => {
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  useEffect(() => {
+    let { selectedCard } = route.params;
+
+    setSelectedCard(selectedCard)
+  },[]);
+
+  function renderHeader() {
+    return (
+      <Header
+        title="CHECKOUT"
+        containerStyle={{
+          height: 50,
+          marginHorizontal: SIZES.padding,
+          marginTop: 40
+        }}
+        leftComponent={
+          <IconButton
+            icon={icons.back}
+            containerStyle={{
+              width: 40,
+              height: 40,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderWidth: 1,
+              borderRadius: SIZES.radius,
+              borderColor: COLORS.gray2
+            }}
+            iconStyle={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.gray2
+            }}
+            onPress={() => navigation.goBack()}
+          />
+        }
+        rightComponent={
+          <View style={{ width: 40}}/>
+        }
+      />
+    )
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.white }}>
-      <Text>Checkout</Text>
+      {/* Header */}
+      {renderHeader()}
+
     </View>
   )
 }

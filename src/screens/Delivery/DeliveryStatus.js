0ed3565b8a189141ca1ebc43b728e01d66fa0276
Jardel Sousa <jardeelsousa@hotmail.com>
2022-03-06 17:14:12 -0300

    Created(Header, Info) in Delivery Status Screen

--- src/screens/Delivery/DeliveryStatus.js
+++ src/screens/Delivery/DeliveryStatus.js
@@ -1,10 +1,59 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
+
+import { Header, LineDivider, TextButton } from '../../components';
+import { FONTS, SIZES, COLORS, icons, constants } from '../../constants';
 
 const DeliveryStatus = ({ navigation }) => {
+
+  function renderHeader() {
+    return (
+      <Header
+        title="DELIVERY STATUS"
+        containerStyle={{
+          height: 50,
+          marginHorizontal: SIZES.padding,
+          marginTop: 40
+        }}
+      />
+    )
+  }
+
+  function renderInfo() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.radius,
+          paddingHorizontal: SIZES.padding
+        }}
+      >
+        <Text style={{ textAlign: 'center',color: COLORS.gray, ...FONTS.body4}}>
+          Estimated Delivery
+        </Text>
+        <Text style={{ textAlign: 'center', ...FONTS.h2 }}>
+          21 Sept 2021 / 12:30PM
+        </Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>DeliveryStatus</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: SIZES.padding,
+        backgroundColor: COLORS.white
+      }}
+    >
+      {/* Header */}
+      {renderHeader()}
+
+      {/* Info */}
+      {renderInfo()}
+
+      {/* Track Order */}
+
+      {/* Footer */}
     </View>
   )
 }

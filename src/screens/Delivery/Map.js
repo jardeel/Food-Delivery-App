7d787849d7c44e15cb7f99bab0b3004740003eb8
Jardel Sousa <jardeelsousa@hotmail.com>
2022-03-07 21:26:11 -0300

    rendering map in MapScreen

--- src/screens/Delivery/Map.js
+++ src/screens/Delivery/Map.js
@@ -1,7 +1,26 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Platform
+} from 'react-native';
+
+import MapView, { Marker } from 'react-native-maps';
+import LinearGradient from 'expo-linear-gradient';
+
+import { IconButton } from '../../components';
+import { COLORS, FONTS, icons, images, SIZES, dummyData, constants } from '../../constants';
+import { utils } from '../../utils';
 
 const Map = ({ navigation }) => {
+  const mapView = useRef();
+  const [region, setRegion] = useState(null);
+  const [toLoc, setToLoc] = useState(null);
+  const [fromLoc, setFromLoc] = useState(null);
+  const [angle, setAngle] = useState(0);
 
   useEffect(() => {
     let initialRegion = {
@@ -15,11 +34,34 @@ const Map = ({ navigation }) => {
       latitude: 1.5496614931250685,
       longitude: 110.36381866919922,
     }
+
+    setToLoc(destination)
+    setFromLoc(dummyData.fromLocs[1])
+
+    setRegion(initialRegion)
   },[])
+
+  function renderMap() {
+    return (
+      <MapView
+        ref={mapView}
+        style={{ flex: 1}}
+        // provider={PROVIDER_GOOGLE}
+        initialRegion={region}
+      >
+
+      </MapView>
+    )
+  }
 
   return (
     <View style={{ flex: 1 }}>
-      <Text>Map</Text>
+      {/* Map */}
+      {renderMap()}
+
+      {/* Header Buttons */}
+
+      {/* Footer / info */}
     </View>
   )
 }
